987750b0c117c005ae538c817e08622898f0e4fa
roombawulf <prydax997@gmail.com>
2023-10-19 14:08:42 +0100

    this one testing the loading live

--- src/ui/loading-screen/LoadingScreen.jsx
+++ src/ui/loading-screen/LoadingScreen.jsx
@@ -1,11 +1,18 @@
 import { useProgress } from "@react-three/drei";
+import "./loadingscreen.scss"
 
 export default function LoadingScreen(){
-    const { progress } = useProgress()
+    const { active, progress, errors, item, loaded, total } = useProgress()
 
     return (
-        <div style={{ position: 'relative', color: 'aliceblue' }}>
-            {progress}
+
+        <div className="loading-screen">
+            <h1 className="loading-value"> progress: {progress} </h1>
+            <h1 className="loading-value"> active: {active ? 'loading...' : 'loaded'} </h1>
+            <h1 className="loading-value"> errors: {errors} </h1>
+            <h1 className="loading-value"> item: {item} </h1>
+            <h1 className="loading-value"> loaded: {loaded} </h1>
+            <h1 className="loading-value"> total: {total} </h1>
         </div>
     )
 }
