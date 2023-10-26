6efa8e47098d7e03f06788ed8f38dc49dc161cca
wulf <prydax997@gmail.com>
2023-10-26 03:14:06 +0100

    nuke the UI, it was messy

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,21 @@
-import { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
+import Scene from "./scene/Scene";
+import UI from "./ui/UI"
+import LoadingScreen from "./ui/loading-screen/LoadingScreen";
 
 import '@fontsource-variable/jost';
 import '@fontsource/space-mono';
 
-import Scene from "./scene/Scene";
-import UI from "./ui/UI"
-import LoadingScreen from "./ui/loading-screen/LoadingScreen";
-import { useUIStore } from "./States";
-
-
 function App() {
-
-    const isMobile = useUIStore((state) => state.isMobile)
-    const setMobile = useUIStore((state) => state.setMobile)
-
-    // const [isMobile, setMobile] = useState(window.innerWidth < 768)
-    const handleResize = () => {
-         setMobile(window.innerWidth < 768)
-    }
-    useEffect(() => {
-        window.addEventListener('resize', handleResize)
-        return () => {window.removeEventListener('resize', handleResize)}
-    },[])
 
     return (
         <>
-            <div className="app-container">
-                <UI />
+            <Canvas className="three-canvas" camera={{fov: 25}}>
+                <Scene />
+            </Canvas>
 
-                <Canvas className={isMobile ? 'mobile' : 'desktop'} camera={{fov: 25}}>
-                    <Scene />
-                </Canvas>
-
-                <LoadingScreen />
-            </div>
-
+            <UI />
+            <LoadingScreen />
         </>
     );
 }
--- src/ui/UI.jsx
+++ src/ui/UI.jsx
@@ -1,50 +1,13 @@
-import { useState, useRef, useLayoutEffect } from "react";
-import { useUIStore } from "../States";
-import { gsap } from "gsap";
-
-import { Curtain } from "./curtain/Curtain";
 import Information from "./information/Information";
-import Navigation from "./navigation/Navigation";
 import SignalScope from "./signalscope/SignalScope";
-import Mute from "./audio-controls/mute/Mute";
-
 import "./ui.scss"
 
 
 function UI() {
 
-    const isMobile = useUIStore((state) => state.isMobile)
-    const [active, setActive] = useState(false)
-    const nav = useRef(null)
-
-    // useLayoutEffect(() => {
-    //     gsap.to(nav.current, {
-    //         height: active ? '100%' : '22px'
-    //     })
-    // }, [active])
-    
-
     return(
         <>
-            <Curtain />
-            <div className={`nav-container ${isMobile ? `m` : null}`}>
-                { isMobile && 
-                        <button 
-                            onClick={() => setActive(!active)}
-                            className="nav-button"
-                        > Click 
-                        </button> 
-                }
-                <nav className={`navbar ${isMobile ? `nav-mobile` : `nav-desktop`}`} ref={nav}>
-                    
-                    <Navigation />
-                    <div style={{ flex: 1 }} />
-                    <Mute />
-                </nav>
-            </div>
-
             <Information />
-
             <SignalScope />
         </>
     )
